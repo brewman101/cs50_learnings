4284961e4b42e1e7d710fb412af10a6118106136
brewman101 <cags.beer@outlook.com>
2024-06-19 13:03:35 +1000

    Dictionary example

---
+++ dictionary function.py
@@ -0,0 +1,15 @@
+# using a dictionary
+# Dictionaries cannot have a clash (two of the same on the left there)
+
+
+
+students={
+    "John": "Year 11",
+    "Tom": "Year 10",
+    "Sally": "Year 11",
+
+}
+
+# reference the student name via characters
+print(students["John"])
+
