92ee24fc47a8188e7b109b07edbb38a53526374b
brewman101 <cags.beer@outlook.com>
2024-06-19 11:47:17 +1000

    using len on lists

---
+++ looping lists.py
@@ -0,0 +1,7 @@
+# This is an example using the len function to count the number of items in a list
+# And then referencing their position in the list.
+
+students=["John","Sarah", "Tom"]
+
+for i in range(len(students)):
+    print(students[i])
