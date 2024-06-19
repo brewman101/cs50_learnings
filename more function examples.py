9118e5ce0a36d554a407d4a6abd9ae353d86fddd
brewman101 <cags.beer@outlook.com>
2024-06-19 11:35:50 +1000

    Working with functions _

---
+++ more function examples.py
@@ -0,0 +1,18 @@
+
+def main():
+    number= get_number()
+    woof(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+def woof(n):
+    for _ in range(n):
+        print("woof")
+
+main()
