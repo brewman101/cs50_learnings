bb4f2ed785ba55513be1176983886706b5a408c2
brewman101 <cags.beer@outlook.com>
2024-06-25 13:24:46 +1000

    Can't seem to get this working to pass the auto checker.

--- grocery list.py
+++ grocery list.py
@@ -1,18 +1,25 @@
 # add items to a grocery list
 # control plus Z exits the loop
+# Note that this seems to work as requested for https://cs50.harvard.edu/python/2022/psets/3/grocery/ but doesn't get accepted.
+# Returns error ":( input of EOF halts program expected exit code 0, not 1"
 
 
-shopping=[]
+shopping={}
 
-def main():
-    while True:
-        try:
-            item=input("testing:")
-            shopping.append(item)
-        except KeyboardInterrupt:
-            break
-        except EOFError:
-            break
-    print(shopping)
 
-main()
+while True:
+    try:
+        item=input("")
+        item=item.upper()
+        check_item=shopping.get(item)
+        if check_item is not None:
+            shopping[item]=(shopping.get(item))+1
+        else:
+            shopping[item]=1
+        sorted_shopping=dict(sorted(shopping.items()))
+    except (KeyboardInterrupt, EOFError):
+        break
+
+print("")
+for key, value in sorted_shopping.items():
+    print(f"{value} {key}")
