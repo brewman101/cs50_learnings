c59ab9c5fb7640eb0231b131acb5c800479868e7
brewman101 <cags.beer@outlook.com>
2024-06-26 13:36:30 +1000

    Learnings from lecture 4 wip

---
+++ Lecture4.py
@@ -0,0 +1,29 @@
+
+# Heads or tails coin flip
+# import random
+# print(random.choice(["Heads","Tails"]))
+
+
+# Random number generator
+# import random
+# number=random.randint(1,1000)
+# print(number)
+
+
+# Shuffle cards
+# import random
+# cards=["jack","queen","king"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+# Calculating a mean
+# import statistics
+# print(statistics.mean([10,20]))
+
+# import sys
+# try:
+#     print("Hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Program requires name")
+
