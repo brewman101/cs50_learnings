b8b9a7a5d5f21f68672785234716070832710539
brewman101 <cags.beer@outlook.com>
2024-06-27 11:29:00 +1000

    Figlet problem solved

--- figlet.py
+++ figlet.py
@@ -5,16 +5,30 @@
 
 
 # Figlet puzzle: https://cs50.harvard.edu/python/2022/psets/4/figlet/
-# What I have so far
+# Completed
 
+
+import sys
 from pyfiglet import Figlet
+figlet = Figlet()
 import random
+font_choice=(sys.argv[2])
+font_list=figlet.getFonts()
+
+if (len(sys.argv)<3):
+    sys.exit("Invalid usage")
+
+if (sys.argv[1]) != "-f":
+    sys.exit("Invalid usage")
+
+if font_choice not in font_list:
+    sys.exit("Invalid usage")
+
 capture=input("Input: ")
 figlet = Figlet()
-random_choice=random.randint(0,549)
-#print(figlet.renderText("string"))
-my_list=(figlet.getFonts())
-choice=my_list[random_choice]
-figlet.setFont(font=choice)
+#random_choice=random.randint(0,549)
+figlet.setFont(font=font_choice)
 print(figlet.renderText(capture))
+
+
 
