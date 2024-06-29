b0543bff64fa1aa8a1fbfca6c6e70be556a4f556
brewman101 <cags.beer@outlook.com>
2024-06-29 20:15:53 +1000

    Lecture 5 wip

---
+++ Lecture5.py
@@ -0,0 +1,12 @@
+# calculator app
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return (n * n)+1
+
+# main won't run by default unless it is specified
+if __name__ == "__main__":
+    main()
---
+++ test_Lecture5.py
@@ -0,0 +1,23 @@
+from Lecture5 import square
+
+def main():
+    test_Lecture5()
+
+def test_Lecture5():
+    # One way I can test my code
+    # if square(2) !=4:
+    #     print("2 squared was not 4")
+    # if square(3) !=9:
+    #     print("3 squared was not 9")
+    # else:
+    #     print("Program working fine")
+    
+    # alternatively I can use the assert function
+    try:
+        assert square(2)==4
+        assert square(4)==16
+    except AssertionError:
+        print("Program has problems")
+
+if __name__ =="__main__":
+    main()
