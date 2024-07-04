64e846104aa71df105e31d15378f36d154926836
brewman101 <cags.beer@outlook.com>
2024-07-04 15:37:30 +1000

    Solution to Pizza Problem.

--- Pizza/pizza.py
+++ Pizza/pizza.py
@@ -2,18 +2,28 @@
 
 import sys
 from tabulate import tabulate
+import csv
+
+
 
 # Check if it has too few arguments
-if sys.argv <2:
+if len(sys.argv) <2:
     sys.exit("Too few command-line arguments")
 # Check if it has too many arguments
-if sys.argv >2:
+if len(sys.argv) >2:
     sys.exit("Too many command-line arguments")
 
 # Check the file exists
-csv=sys.argv[1]
+csvFile=sys.argv[1]
+if csvFile.endswith(".csv") == False:
+    sys.exit("Not a CSV file")
+
 try:
-    open(csv, "r")
+    open(csvFile, "r")
 except FileNotFoundError:
     sys.exit("File does not exist")
 
+file = open(csvFile, "r")
+data = list(csv.reader(file, delimiter=","))
+file.close()
+print(tabulate(data[1:],data[0],tablefmt="grid"))
