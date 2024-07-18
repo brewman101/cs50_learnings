12e004335c2ac0b957ce6508963a7217e6238b55
brewman101 <cags.beer@outlook.com>
2024-07-18 13:51:07 +1000

    9 to 5 wip 4

--- 9 to 5/9 to 5.py
+++ 9 to 5/9 to 5.py
@@ -2,31 +2,45 @@ import re
 import sys
 
 # Capture hours
-#input="10:00 AM to 10:00 PM"
-amount=input("Hours: ")
+amount="5 AM to 10 PM"
+#amount=input("Hours: ")
 
 # Check that it meets correct formatting
 # XX:XX AM to XX:XX PM
-times=(re.search(r"([0-9]+):([0-9]+) ([A|P][M]) to ([0-9]+):([0-9]+) ([A|P][M])",amount))
-if times:
-
-    if int(times.group(2))>59:
+times=(re.search(r"([0-9]+)(:)?([0-9]+)? ([A|P][M]) to ([0-9]+)(:)?([0-9]+)? ([A|P][M])",amount))
+if times and times.group(2)!=None:
+    if int(times.group(3))>59:
         raise ValueError
-    if int(times.group(5))>59:
+    else:
+        b=int(times.group(3))
+    if int(times.group(7))>59:
         raise ValueError
-    if times.group(3)=="PM":
+    else:
+        d=int(times.group(7))
+    if times.group(4)=="PM":
         a=int(times.group(1))+12
     else:
         a=int(times.group(1))
-    if times.group(6)=="PM":
-        c=int(times.group(4))+12
+    if times.group(8)=="PM":
+        c=int(times.group(5))+12
     else:
-        c=int(times.group(4))
+        c=int(times.group(5))
 
+elif times and times.group(2)==None:
+    if times.group(4)=="PM":
+        a=int(times.group(1))+12
+    else:
+        a=int(times.group(1))
+    if times.group(8)=="PM":
+        c=int(times.group(5))+12
+    else:
+        c=int(times.group(5))
+    b=int(00)
+    d=int(00)
 
 else:
     raise ValueError
 
 
-print(f"{a:02d}:{times.group(2)} to {c:02d}:{times.group(5)}")
+print(f"{a:02d}:{b:02d} to {c:02d}:{d:02d}")
 
--- test.py
+++ test.py
@@ -10,3 +10,14 @@
 # string = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 # print("Urls: ", Find(string))
 
+import re
+import sys
+
+# Capture hours
+amount="5 AM to 10 PM"
+#amount=input("Hours: ")
+
+# Check that it meets correct formatting
+# XX:XX AM to XX:XX PM
+times=(re.search(r"([0-9]+)(:)?([0-9]+)? ([A|P][M]) to ([0-9]+)(:)?([0-9]+)? ([A|P][M])",amount))
+print(times.group(8))
