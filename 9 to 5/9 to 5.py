dfb32660bb69a2bf7934b5320b8ea83b3e602db9
brewman101 <cags.beer@outlook.com>
2024-07-18 14:58:58 +1000

    9 to 5 completed program part.

--- 9 to 5/9 to 5.py
+++ 9 to 5/9 to 5.py
@@ -2,45 +2,61 @@ import re
 import sys
 
 # Capture hours
-amount="5 AM to 10 PM"
-#amount=input("Hours: ")
+
+def main():
+    print(convert(input("Hours: ")))
+
 
 # Check that it meets correct formatting
 # XX:XX AM to XX:XX PM
-times=(re.search(r"([0-9]+)(:)?([0-9]+)? ([A|P][M]) to ([0-9]+)(:)?([0-9]+)? ([A|P][M])",amount))
-if times and times.group(2)!=None:
-    if int(times.group(3))>59:
+def convert(s):
+    s=(re.search(r"([0-9]+)(:)?([0-9]+)? ([A|P][M]) to ([0-9]+)(:)?([0-9]+)? ([A|P][M])",s))
+    if s and s.group(2)!=None:
+        a=int(s.group(1))
+        c=int(s.group(5))
+        if int(s.group(3))>59:
+            raise ValueError
+        else:
+            b=int(s.group(3))
+        if int(s.group(7))>59:
+            raise ValueError
+        else:
+            d=int(s.group(7))
+        if s.group(4)=="PM" and a!=12:
+            a=int(s.group(1))+12
+        else:
+            a=int(s.group(1))
+        if s.group(8)=="PM" and c!=12:
+            c=int(s.group(5))+12
+        else:
+            c=int(s.group(5))
+        if a==12 and s.group(4)=="AM":
+            a=0
+        elif c==12 and s.group(8)=="AM":
+            a=0
+    elif s and s.group(2)==None:
+        a=int(s.group(1))
+        c=int(s.group(5))
+        if s.group(4)=="PM" and a!=12:
+            a=int(s.group(1))+12
+        else:
+            a=int(s.group(1))
+        if s.group(8)=="PM" and c!=12:
+            c=int(s.group(5))+12
+        else:
+            c=int(s.group(5))
+        if a==12 and s.group(4)=="AM":
+            a=0
+        elif c==12 and s.group(8)=="AM":
+            a=0
+        b=int(00)
+        d=int(00)
+
+    else:
         raise ValueError
-    else:
-        b=int(times.group(3))
-    if int(times.group(7))>59:
-        raise ValueError
-    else:
-        d=int(times.group(7))
-    if times.group(4)=="PM":
-        a=int(times.group(1))+12
-    else:
-        a=int(times.group(1))
-    if times.group(8)=="PM":
-        c=int(times.group(5))+12
-    else:
-        c=int(times.group(5))
 
-elif times and times.group(2)==None:
-    if times.group(4)=="PM":
-        a=int(times.group(1))+12
-    else:
-        a=int(times.group(1))
-    if times.group(8)=="PM":
-        c=int(times.group(5))+12
-    else:
-        c=int(times.group(5))
-    b=int(00)
-    d=int(00)
+    s=(f"{a:02d}:{b:02d} to {c:02d}:{d:02d}")
+    return s
 
-else:
-    raise ValueError
-
-
-print(f"{a:02d}:{b:02d} to {c:02d}:{d:02d}")
-
+if __name__ == "__main__":
+    main()
