c5b76a6cfce48f6fa40b5bf21a5394e4a6632901
brewman101 <cags.beer@outlook.com>
2024-08-02 14:50:52 +1000

    Solution to jar problem.

--- jar/jar.py
+++ jar/jar.py
@@ -7,21 +7,22 @@ cookie=emoji.emojize(':cookie:')
 class Jar:
     def __init__(self, capacity=12):
         if capacity<0:
-            sys.exit(ValueError)
+            raise ValueError ("Capacity cannot be negative")
         self._capacity=capacity
         self._size=0
 
+
     def __str__(self):
         return f"{self.size * cookie}"
-    
+
     def deposit(self, n):
         if n + self.size > self.capacity:
-            sys.exit(ValueError)
+            raise ValueError("Deposit exceeds capacity")
         self._size=self.size + n
-      
+
     def withdraw(self, n):
         if self._size - n < 0:
-            sys.exit(ValueError)
+            raise ValueError("You have removed too many cookies")
         self._size=self.size-n
 
 
@@ -29,11 +30,11 @@ class Jar:
     def capacity(self):
         return self._capacity
 
+    @capacity.setter
+    def capacity(self, value):
+        self._capacity = value
+
     @property
     def size(self):
         return self._size
 
-
-jar=Jar()
-jar.deposit(1)
-print(jar)
--- jar/test_jar.py
+++ jar/test_jar.py
@@ -4,3 +4,15 @@ from jar import Jar
 def test1():
     jar=Jar()
     assert str(jar)==""
+def test2():
+    jar=Jar()
+    jar.deposit(1)
+    assert str(jar)=="🍪"
+def test3():
+    jar=Jar()
+    jar.deposit(3)
+    assert str(jar)=="🍪🍪🍪"
+def test4():
+    jar=Jar()
+    jar.capacity = 10
+    assert str(jar)==""
