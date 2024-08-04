a69c7dfdc3342b34d0f86b389a886e2917e0f613
brewman101 <cags.beer@outlook.com>
2024-08-04 21:07:53 +1000

    shirtificate solution

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,6 +6,10 @@ class PDF():
         self._pdf.add_page()
         self._pdf.set_font("helvetica", "B", 50)
         self._pdf.cell(0, 60 , 'CS50 Shirtificate' , new_x="LMARGIN", new_y="NEXT", align='C')
+        self._pdf.image("shirtificate.png", w=self._pdf.epw)
+        self._pdf.set_font_size (25)
+        self._pdf.set_text_color(255, 255, 255)
+        self._pdf.text(x=48, y=140, text=f"{name} took CS50")
 
     def save(self, name):
         self._pdf.output(name)
